59de0db4c871cef6d4f2335c500295056cf33295
Grasee <grase1145@gmail.com>
2020-03-22 14:44:29 +0800

    前往指定元素

--- GoToElement.js
+++ GoToElement.js
@@ -3,12 +3,24 @@ $("*").each(function () {
         var target = $(this).attr("data-goto-target");
         
         if (target != undefined){
+            // 時間屬性
             var duration = $(this).attr("data-goto-duration");
+
+            // 上方位置 = 目標屬性.位移.上方
             var top = $(target).offset().top;
-            
+
+            // 先停止所有動畫 再執行動畫({捲動上方：上方位置}，時間)
             $("html, body").animate({scrollTop: top}, parseInt(duration));
         }
 
     });
     
+});
+
+// 當捲動滾輪時停止動畫
+// jqon 事件：mousewheel 滑鼠滾動
+$(window).on("mousewheel" ,function () {
+    $("*").each(function () {
+        $(this).stop();     
+    });
 });
